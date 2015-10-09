a9d899eb96a72b65adaeabb567a390e8f0293518
mul <niefee@qq.com>
2015-10-09 22:33:49 +0800

    记录
    完成 了顶部cookie的部分。

--- js/index.js
+++ js/index.js
@@ -1,5 +1,69 @@
+/*页面加载*/
+window.onload=function(){
+	date=new Date();
+	expiresDays=365;
+	date.setTime(date.getTime()+expiresDays*24*3600*1000)
+
+	
+}
+
+/*头部提示条*/
 var rightNotice=document.querySelector(".right-unnotice");
 var notice=document.querySelector(".u-notice");
-rightNotice.onclick=function(){
+
+rightNotice.onclick=function(){	
+
+	setCookie("isInfo","bu",date);
 	notice.style.display="none";
 }
+//检查cookie,顶部“不再提示”事件。
+
+if(getCookie("isInfo")){//检测是否出现通知栏
+
+		notice.style.display='none';
+	}
+//思考上下两个有什么区别？
+/*function ifShow(){	
+	if(getCookie("isInfo")){
+
+		notice.style.display='none';
+	}
+}
+ifShow();*/
+
+/*读取cookie*/
+function getCookie (Name) {
+	var cookie={};
+	var  all=document.cookie;
+	if(all==='')
+		return cookie;
+	var list=all.split(';');
+	for(var i=0;i<list.length;i++){
+		var item=list[i];
+		var p=item.indexOf('=');
+		var name=item.substring(0,p);
+		name=decodeURIComponent(name);
+		var value=item.substring(p+1);
+		value=decodeURIComponent(value);
+		cookie[name]=value;
+	}
+	return cookie[Name];
+}
+/*设置cookie*/
+function setCookie(name,value,expires,path,domain,secure){
+	var cookie=encodeURIComponent(name)+"="+encodeURIComponent(value);
+	if(expires)
+		cookie+=';expires='+expires.toGMTString();
+	if(path)
+		cookie+=';path='+path;
+	if(domain)
+		cookie+=';domain='+domain;
+	if(secure)
+		cookie+=';secure='+secure;
+	document.cookie=cookie;
+}
+//删除cookie
+function removeCookie(name,path,domain){
+	document.cookie=name+'='+';path='+path+';domain='+domain+';max-age=0';
+}
+
